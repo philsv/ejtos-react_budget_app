de6c328191139b2ccffeff167b49b6a2a73f0e66
philsv <35413649+philsv@users.noreply.github.com>
2023-08-16 18:29:16 +0200

    refactor: improve code quality

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,5 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useMemo } from 'react';
 
-// 5. The reducer - this is used to update the state, based on the action
 export const AppReducer = (state, action) => {
     let budget = 0;
     switch (action.type) {
@@ -11,7 +10,7 @@ export const AppReducer = (state, action) => {
                     return previousExp + currentExp.cost
                 },0
             );
-            total_budget = total_budget + action.payload.cost;
+            total_budget += action.payload.cost;
             action.type = "DONE";
             if(total_budget <= state.budget) {
                 total_budget = 0;
@@ -33,7 +32,7 @@ export const AppReducer = (state, action) => {
             case 'RED_EXPENSE':
                 const red_expenses = state.expenses.map((currentExp)=> {
                     if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
-                        currentExp.cost =  currentExp.cost - action.payload.cost;
+                        currentExp.cost -= action.payload.cost;
                         budget = state.budget + action.payload.cost
                     }
                     return currentExp
@@ -89,33 +88,30 @@ const initialState = {
     currency: '£'
 };
 
-// 2. Creates the context this is the thing our components import and use to get the state
 export const AppContext = createContext();
-
-// 3. Provider component - wraps the components we want to give access to the state
-// Accepts the children, which are the nested(wrapped) components
 export const AppProvider = (props) => {
-    // 4. Sets up the app state. takes a reducer, and an initial state
     const [state, dispatch] = useReducer(AppReducer, initialState);
-    let remaining = 0;
 
-    if (state.expenses) {
+    const remaining = useMemo(() => {
+        if (state.expenses) {
             const totalExpenses = state.expenses.reduce((total, item) => {
-            return (total = total + item.cost);
-        }, 0);
-        remaining = state.budget - totalExpenses;
-    }
+                return (total += item.cost);
+            }, 0);
+            return state.budget - totalExpenses;
+        }
+        return 0;
+    }, [state.expenses, state.budget]);
+
+    const contextValue = useMemo(() => ({
+        expenses: state.expenses,
+        budget: state.budget,
+        remaining: remaining,
+        dispatch,
+        currency: state.currency
+    }), [state.expenses, state.budget, remaining, dispatch, state.currency]);
 
     return (
-        <AppContext.Provider
-            value={{
-                expenses: state.expenses,
-                budget: state.budget,
-                remaining: remaining,
-                dispatch,
-                currency: state.currency
-            }}
-        >
+        <AppContext.Provider value={contextValue}>
             {props.children}
         </AppContext.Provider>
     );
